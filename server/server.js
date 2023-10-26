c15e00848cf610c4eb26b233457f8336142c6342
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2023-10-27 03:58:46 +0800

    connected to DB

--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
+
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
