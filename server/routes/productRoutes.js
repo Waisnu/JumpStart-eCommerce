cad74b5c84b97476eb7f56e2b45ee16e36c16fe2
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2023-10-27 05:58:15 +0800

    added ErrorHandling

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,24 +1,18 @@
 import express from 'express'
+
 const router = express.Router()
-import asyncHandler from '../middleware/asyncHandler.js'
-import Product from '../models/productModel.js'
+import { 
+    getProducts, 
+    getProductById } from '../controllers/productController.js'
+    
 
 
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
-
-router.get('/:id',asyncHandler(async (req, res) => {
-    const product = await Product.findbyId(req.params.id)
-
-    if(product ) {
-        res.json(product)    
-    }
-
-    res.status(404).json({message: 'Product not found!'})
-}))
 
 
 export default router
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 import express from 'express'
+import { 
+    notFound, 
+    errorHandler } from '../server/middleware/errorMiddleware.js'
+
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -6,7 +10,7 @@ dotenv.config()
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
-import products from './data/products.js'
+
 
 const port = process.env.PORT || 5000
 
@@ -19,6 +23,8 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 
+app.use(notFound)
+app.use(errorHandler)
 
 
 
