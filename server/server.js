873bdb81afc7de3481222fce124e98ae200f00cb
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2023-10-27 04:36:49 +0800

    fetching data from db

--- server/server.js
+++ server/server.js
@@ -4,27 +4,22 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import connectDB from './config/db.js'
-import products from './data/products.js'
 
+import productRoutes from './routes/productRoutes.js'
+import products from './data/products.js'
 
 const port = process.env.PORT || 5000
 
 connectDB()
-
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('this is the API running on this port')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+
 
 
 app.listen(port, () => {
