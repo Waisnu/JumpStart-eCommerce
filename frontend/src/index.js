1a231da37bfc96f4c4a805ed79f2ea353787768c
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2023-10-28 06:37:46 +0800

    Production Ready

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/bootstrap.min.css';
 import './assets/styles/index.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
